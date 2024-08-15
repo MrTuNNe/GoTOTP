1e5852d6a1e0d63442834283489762f0a93d3916
Groza Samuel <contact@mrtunne.info>
2024-08-15 14:55:49 +0200

    Added `GenerateRandomSecret()`

---
+++ utils.go
@@ -0,0 +1,18 @@
+package totp
+
+import (
+	"crypto/rand"
+	"encoding/base32"
+)
+
+// This will generate random bytes and encode them to Base32 (without padding)
+func GenerateRandomSecret(length int) (string, error) {
+	secret := make([]byte, length)
+	_, err := rand.Read(secret)
+	if err != nil {
+		return "", err
+	}
+	b32enc := base32.StdEncoding.WithPadding(base32.NoPadding)
+	result := b32enc.EncodeToString(secret)
+	return result, nil
+}
